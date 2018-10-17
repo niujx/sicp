e118dc11715c954675274c38542829986df586d8
niujiaxi <68394845@163.com>
2018-10-17 17:47:54 +0800

    更新题

---
+++ chapter-1/1.18.rkt
@@ -0,0 +1,15 @@
+#lang scheme
+# 俄罗斯农民法
+(define (russia a b c)
+   (cond ((= b 0) c)
+         ((even? b) (russia ( * a 2) (div b) c))
+         (else  (russia ( * a 2) (div b) (+ c a)))))
+
+(define (even? n)(= (remainder n 2) 0))
+
+(define (div n) 
+    (if (even? n) (/ n 2) (/ (- n 1) 2)))
+
+
+(russia 2 3 0)
+
