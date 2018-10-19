34319cfbf0a7fabec4cf874a7d391b833c437e7f
niujiaxi <68394845@163.com>
2018-10-19 17:19:57 +0800

    修改一个bug

---
+++ chapter-1/fermat-test.rkt
@@ -0,0 +1,35 @@
+#lang scheme
+
+#| 费马检查
+|#
+
+(define (even? n)(= (remainder n 2) 0))
+
+(define (square x) (* x x))
+
+#|
+ 一个数a的幕对另外一个数b取摸 如果等于b 则b很有可能是素数
+|#
+(define (expmod base exp m)
+   (cond ((= exp 0) 1)
+         ((even? exp) 
+            (remainder (square (expmod base (/ exp 2) m)) m))
+         (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+
+(define (fermat-test n)
+
+   (define (try-it a)
+      (= (expmod a n n) a))
+
+(try-it (+ 1 (random (- n 1)))))
+
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false))
+
+)
+
+(fast-prime? 12222 3)
--- chapter-1/prime.rkt
+++ chapter-1/prime.rkt
@@ -4,15 +4,21 @@
     (= n (prime?-iter n 2)))
 
 (define (prime?-iter n test)
-    (cond ((> test n) n)
-          ((check n (sequare test )) test )
+    (cond ((> (sequare test) n) n)
+          ((check  n test) test )
           (else (prime?-iter n (+ test 1))))
 )
 
-(define (check n test)(= (remainder test n ) 0))
+(define (check n test)(= (remainder n test) 0))
 
 (define (sequare x)(* x x))
 
 (prime? 4)
 
 (prime? 5)
+
+(prime?-iter 199 2)
+
+(prime?-iter 1999 2)
+
+(prime?-iter 19999 2)
